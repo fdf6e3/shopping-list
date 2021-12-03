475e07b9b296dfcd18cbb693bbeaf5ea14459a44
YOUNGHYUN BAE <yhbae97@gmail.com>
2021-12-04 00:22:28 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+
+function onAdd() {
+  const text = input.value;
+  if (text === '') {
+    input.focus();
+    return;
+  };
+
+  const item = createItem(text);
+
+  items.appendChild(item);
+
+  input.value = '';
+  input.focus();
+};
+
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item__name');
+  name.innerText = text;
+
+  const delBtn = document.createElement('button');
+  delBtn.setAttribute('class', 'item__delete')
+  delBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  delBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item__divider');
+
+  item.appendChild(name);
+  item.appendChild(delBtn);
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
+};
+
+addBtn.addEventListener('click', () => {
+  onAdd();
+});
